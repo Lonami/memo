71e3471aeab26139f9c7e5bedf21bce4da9c7939
Lonami Exo <totufals@hotmail.com>
2021-02-07 12:57:22 +0100

    Enumerate process identifiers

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,28 @@
+use std::io;
+use std::mem;
+use winapi::shared::minwindef::{DWORD, FALSE};
+
+pub fn enum_proc() -> io::Result<Vec<u32>> {
+    let mut size = 0;
+    let mut pids = Vec::<DWORD>::with_capacity(1024);
+    // SAFETY: the pointer is valid and the size matches the capacity.
+    if unsafe {
+        winapi::um::psapi::EnumProcesses(
+            pids.as_mut_ptr(),
+            (pids.capacity() * mem::size_of::<DWORD>()) as u32,
+            &mut size,
+        )
+    } == FALSE
+    {
+        return Err(io::Error::last_os_error());
+    }
+
+    let count = size as usize / mem::size_of::<DWORD>();
+    // SAFETY: the call succeeded and count equals the right amount of items.
+    unsafe { pids.set_len(count) };
+    Ok(pids)
+}
+
 fn main() {
+    dbg!(enum_proc().unwrap().len());
 }
