452471494bc9f3a5da343edbd0862b88be9f4304
Lonami Exo <totufals@hotmail.com>
2021-10-29 20:09:47 +0200

    Add missing includes at root module

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 pub mod ffi;
 pub mod scanner;
+mod serdes;
 
 pub use ffi::{debug_process, iter_threads, list_processes, Process, Thread};
-pub use scanner::{take_memory_snapshot, PointerPathFinder, Scan};
+pub use scanner::{take_memory_snapshot, PointerPathFinder, Scan, Snapshot};
+use serdes::SerDes;
